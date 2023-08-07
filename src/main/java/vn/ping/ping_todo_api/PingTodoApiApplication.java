6ba71ccb0d71a6bf4caba2b76416d9c7e7203c47
nguyenminhvuong <minhvuong06082001@gmail.com>
2023-08-07 22:00:48 +0700

    create init project

---
+++ src/main/java/vn/ping/ping_todo_api/PingTodoApiApplication.java
@@ -0,0 +1,13 @@
+package vn.ping.ping_todo_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PingTodoApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PingTodoApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/vn/ping/ping_todo_api/PingTodoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.ping.ping_todo_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PingTodoApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
